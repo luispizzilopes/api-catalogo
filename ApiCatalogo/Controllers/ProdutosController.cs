4a542bc0d5cf569a62a8bea94213b53c1adb1d23
Luis Felipe Pizzi Lopes <luisfelipe1203lf@gmail.com>
2023-03-13 16:12:36 -0300

    Atualização ProdutosController

--- ApiCatalogo/Controllers/ProdutosController.cs
+++ ApiCatalogo/Controllers/ProdutosController.cs
@@ -29,5 +29,32 @@ namespace ApiCatalogo.Controllers
                 return NotFound("Produtos não encontrados!"); 
             }
         }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<Produto> Get(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+            if(produto != null)
+            {
+                return produto;
+            }
+            else
+            {
+                return NotFound("Produto com o id informado não encontrado!"); 
+            }
+        }
+
+        [HttpGet("{nome}")]
+        public ActionResult<Produto> Get(string nome)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.Nome == nome);   
+            if(produto != null )
+            {
+                return produto; 
+            }
+            {
+                return NotFound("Produto com o nome informado não encontrado!");
+            }
+        }
     }
 }
