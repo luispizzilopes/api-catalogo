cd8082ae1c18e84a9a8fa3a0608dab99ffc1ea34
Luis Felipe Pizzi Lopes <luisfelipe1203lf@gmail.com>
2023-04-04 08:27:31 -0300

    Implementação Unit Of Work

---
+++ ApiCatalogo/Repository/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using ApiCatalogo.Repository.Interfaces;
+
+namespace ApiCatalogo.Repository.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        IProdutoRepository ProdutoRepository { get; }
+        ICategoriaRepository CategoriaRepository { get; }
+        void Commit(); 
+    }
+}
